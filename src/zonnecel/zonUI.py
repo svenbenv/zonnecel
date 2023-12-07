15b2001090b21937ac8ec429c195c307062b4b5d
limeng-111 <68026174+limeng-111@users.noreply.github.com>
2023-12-07 12:04:09 +0100

    GUI initialized

--- src/zonnecel/zonUI.py
+++ src/zonnecel/zonUI.py
@@ -19,8 +19,6 @@ class UserInterface(QtWidgets.QMainWindow):
         self.plot_widget = pg.PlotWidget()
         central_widget = QtWidgets.QWidget()
         self.setCentralWidget(central_widget)
-        self.plot_widget.setLabel("left", "Voltage in V")
-        self.plot_widget.setLabel("bottom", "Current in A")
 
         # central layout
         vbox = QtWidgets.QVBoxLayout(central_widget)
@@ -30,12 +28,101 @@ class UserInterface(QtWidgets.QMainWindow):
         hbox1 = QtWidgets.QHBoxLayout()
         hbox2 = QtWidgets.QHBoxLayout()
         hbox3 = QtWidgets.QHBoxLayout()
-        hbox4 = QtWidgets.QHBoxLayout()
+        hboxcombo = QtWidgets.QHBoxLayout()
         hboxpoortlabel = QtWidgets.QHBoxLayout()
-        vbox.addLayout(hbox1)
         vbox.addLayout(hbox)
+        vbox.addLayout(hbox1)
         vbox.addLayout(hboxpoortlabel)
+        vbox.addLayout(hboxcombo)
         vbox.addLayout(hbox2)
         vbox.addLayout(hbox3)
-        vbox.addLayout(hbox4)
 
+        #button
+        self.start_value = QtWidgets.QDoubleSpinBox()
+        self.start_value.setRange(0,10)
+        self.start_value.setSingleStep(0.1)
+        self.start_value.setValue(0)
+        hbox1.addWidget(self.start_value)
+
+        self.stop_value = QtWidgets.QDoubleSpinBox()
+        # label stoplabel = QLabel()
+        self.stop_value.setRange(0,10)
+        self.stop_value.setSingleStep(0.1)
+        self.stop_value.setValue(10)
+        hbox1.addWidget(self.stop_value)
+
+        self.repeat_times = QtWidgets.QSpinBox()
+        self.repeat_times.setValue(1)
+        self.repeat_times.setRange(1, 30)
+        hbox1.addWidget(self.repeat_times)
+
+        graph_button = QtWidgets.QPushButton("graph")
+        hbox2.addWidget(graph_button)
+
+        clear_button = QtWidgets.QPushButton("clear")
+        hbox2.addWidget(clear_button)
+
+        save_button = QtWidgets.QPushButton("save")
+        hbox3.addWidget(save_button)
+
+        Quit_button = QtWidgets.QPushButton("quit")
+        hbox3.addWidget(Quit_button)
+
+        self.selectplot = QtWidgets.QComboBox()
+        item = ["UI", "PR"]
+        self.selectplot.addItems(item)
+        hboxcombo.addWidget(self.selectplot)
+
+
+        #poort comboButton
+        # self.selectAD = QtWidgets.QComboBox()
+        # poort = list_devices()
+        # for item in poort:
+        #     self.selectAD.addItem(item)
+        # hbox2.addWidget(self.selectAD)
+
+        #Push buttons
+
+    
+        #labels
+        label_start = QtWidgets.QLabel("start")
+        hbox.addWidget(label_start)
+
+        label_stop = QtWidgets.QLabel("stop")
+        hbox.addWidget(label_stop)
+
+        label_repeat = QtWidgets.QLabel("times repeat")
+        hbox.addWidget(label_repeat)
+
+
+
+        #clicked
+
+#@Slot
+    def UI_plot():
+        """plot UI-diagram
+        """
+        # self.plot_widget.setLabel("left", "Voltage in V")
+        # self.plot_widget.setLabel("bottom", "Current in A")
+    
+    def save_data(self):
+        """
+        Save measured data in Csv-sile
+        """
+        filename, _ = QtWidgets.QFileDialog.getSaveFileName(filter="CSV files (*.csv)")
+        print(filename)
+        with open(f"{filename}", "w", newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["V", "A","V_err","A_err"])
+            for a, b, c, d in zip(self.voltageLED, self.amperage, self.Verror, self.Aerror):
+                writer.writerow([a, b, c, d])
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    ui = UserInterface()
+    ui.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()  
