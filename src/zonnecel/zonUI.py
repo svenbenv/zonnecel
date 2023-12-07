d2899aa47a71a26f6e96f59834c164b7df546d09
limeng-111 <68026174+limeng-111@users.noreply.github.com>
2023-12-07 11:46:20 +0100

    1

--- src/zonnecel/zonUI.py
+++ src/zonnecel/zonUI.py
@@ -5,9 +5,37 @@ import pyqtgraph as pg
 from PySide6 import QtWidgets
 from PySide6.QtCore import Slot
 
-from pythondaq.DiodeExperiment import DiodeExperiment,list_devices
+#from  import 
 
 
 # PyQtGraph global options
 pg.setConfigOption("background", "w")
 pg.setConfigOption("foreground", "k")
+
+class UserInterface(QtWidgets.QMainWindow):
+    def __init__(self):
+       
+        super().__init__()
+        self.plot_widget = pg.PlotWidget()
+        central_widget = QtWidgets.QWidget()
+        self.setCentralWidget(central_widget)
+        self.plot_widget.setLabel("left", "Voltage in V")
+        self.plot_widget.setLabel("bottom", "Current in A")
+
+        # central layout
+        vbox = QtWidgets.QVBoxLayout(central_widget)
+        # self.textedit = QtWidgets.QTextEdit()
+        vbox.addWidget(self.plot_widget)
+        hbox = QtWidgets.QHBoxLayout()
+        hbox1 = QtWidgets.QHBoxLayout()
+        hbox2 = QtWidgets.QHBoxLayout()
+        hbox3 = QtWidgets.QHBoxLayout()
+        hbox4 = QtWidgets.QHBoxLayout()
+        hboxpoortlabel = QtWidgets.QHBoxLayout()
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox)
+        vbox.addLayout(hboxpoortlabel)
+        vbox.addLayout(hbox2)
+        vbox.addLayout(hbox3)
+        vbox.addLayout(hbox4)
+
